8ed449d396016f1870968df481b7f46444867230
bigbeddes <bigbeddes@gmail.com>
2023-01-07 14:55:15 -0800

    completed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+
+        Console.Write("What is your grade percentage? (example- 98) ");
+        string grade = Console.ReadLine();
+        int numberGrade = int.Parse(grade);
+
+        if (numberGrade >= 90)
+        {
+            letter="A";
+        }
+        else if (numberGrade >= 80)
+        {
+            letter="B";
+        }
+        else if (numberGrade >= 70)
+        {
+            letter="C";
+        }
+        else if (numberGrade >= 60)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter="F";
+        }
+
+        if (numberGrade>=70)
+        {
+            Console.WriteLine($"You got a {letter}. Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine($"You got a {letter}. You didn't pass, please try again.");
+        }
+        
     }
 }
