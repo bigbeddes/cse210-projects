0f566f550328934f582a41a1bc779f3ecba585b0
bigbeddes <bigbeddes@gmail.com>
2023-01-16 21:36:36 -0800

    Job History Week 2 Assignment completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1 = new Job();
+       job1._company="City of Sacramento";
+       job1._jobTitle="Information Technology Support Specialist";
+       job1._startYear=2014;
+       job1._endYear=2019;
+
+       Job job2 = new Job();
+       job2._company="California Highway Patrol";
+       job2._jobTitle="Information Technology  Specialist";
+       job2._startYear=2019;
+       job2._endYear=2023;
+
+       Resume myResume= new Resume();
+       myResume._name="Ryan Beddes";
+
+       myResume._jobs.Add(job1);
+       myResume._jobs.Add(job2);
+
+       myResume.Display();
     }
 }
